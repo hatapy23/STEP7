a842afc3d883e7157240fc120d037c9b47185ba8
sayaka hatada <mandarino@sayakanoMacBook-Pro.local>
2025-08-01 07:27:26 +0900

    マージコンフリクトの解消

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,15 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany; 
+
 class Company extends Model{
     //DBからテーブルのデータを取得
     public function getList() {
+        protected $fillable = [
+            'company_name',
+            // 'address', 'phone_number' など、companiesテーブルに存在するカラムを記述
+        ];
         $companies = DB::table('companies')
         ->join('products', 'company_id', '=', 'companies.id')
         ->select('products.*', 'companies.company_name')
--- database/migrations/2024_10_02_130015_create_products_table.php
+++ database/migrations/2024_10_02_130015_create_products_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('price'); 
             $table->integer('stock'); 
             $table->text('comment');
-            $table->string('img_path');  
+            $table->string('img_path')->nullable;  
             $table->timestamps();});
      }
     /**
