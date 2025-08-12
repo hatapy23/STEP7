0240cd3e69e4f68953b7fea289e59d2578324f3c
sayaka hatada <mandarino@sayakanoMacBook-Pro.local>
2025-08-12 22:02:28 +0900

    バリデーションルールの修正

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -20,12 +20,11 @@ class ProductRequest extends FormRequest
      */
     public function rules(){
         return [
-            'product_name' => ['required', 'max:255'],
-            'company_name' => ['required', 'max:255'],
-            'price' => ['required', 'numeric', 'min:0', 'half_width_numeric'],
-            'stock' => ['required', 'integer', 'min:0', 'half_width_numeric'],
+            'product_name' => 'required|max:255',
+            'company_name' => 'required|max:255',
+            'price' => 'required|integer|numeric',
+            'stock' => 'required|integer|numeric',
             'comment' => 'nullable|max:10000',
-            'img_path' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ];
     }
     public function attributes(){
@@ -34,8 +33,7 @@ class ProductRequest extends FormRequest
             'company_name' => 'メーカー名',
             'price' => '価格',
             'stock' => '在庫数',
-            'comment' => 'コメント',
-            'img_path' => '商品画像',
+            'comment' => 'コメント'
         ];
     }
     public function messages(){
@@ -45,15 +43,10 @@ class ProductRequest extends FormRequest
             'company_name.required' => ':attributeは必須項目です。',
             'company_name.max' => ':attributeは:max字以内で入力してください。',
             'price.required' => ':attributeは必須項目です。',
-            'price.numeric' => ':attributeは数値で入力してください。',
-            'price.min' => ':attributeは0以上で入力してください。',
-            'price.half_width_numeric' => ':attributeは半角数字のみで入力してください。',
+            'price.integer' => ':attributeは整数で入力してください。',
             'stock.required' => ':attributeは必須項目です。',
             'stock.integer' => ':attributeは整数で入力してください。',
-            'stock.min' => ':attributeは0以上で入力してください。',
-            'stock.half_width_numeric' => ':attributeは半角数字のみで入力してください。',
             'comment.max' => ':attributeは:max字以内で入力してください。',
-            'img_path' => ':attributeは:max以下のものをアップロードしてください。'
         ];
     }
 }
